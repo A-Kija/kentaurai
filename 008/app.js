88a2423cacf333737df1849ccf199aa1a589f683
A-Kija <arvydas.kijakauskas@gmail.com>
2024-03-07 15:32:45 +0200

    008

---
+++ 008/app.js
@@ -0,0 +1,29 @@
+console.log('Hello from far away!');
+
+
+let g;
+
+let a = -50;
+let b = '';
+let c = 'Hello';
+
+let x = 'An';
+let y = 'tis';
+
+let l1 = 'B';
+let l2 = 'a';
+
+// console.log((0.3 + 0.6).toFixed(5));
+
+console.log(0.3 + 0.6);
+
+a = 50;
+
+console.log(g + b);
+
+let t = true;
+let f = false;
+
+
+console.log(t || f);
+
