7567f87a8732e1782a43009e4a4a94f763f48bf5
A-Kija <arvydas.kijakauskas@gmail.com>
2024-03-19 16:46:53 +0200

    013

--- 013/app.js
+++ 013/app.js
@@ -71,3 +71,106 @@ for (let i = 0; i < 5; i++) {
 }
 
 console.log('Suma:', sum7);
+
+console.clear();
+
+let sviesoforas = rand(0, 2);
+let spalva = sviesoforas === 0 ? 'raudona' : sviesoforas === 1 ? 'geltona' : 'zalia';
+
+console.log('Sviesoforas:', spalva);
+
+if (spalva === 'raudona') {
+    console.log('Stovime');
+} else if (spalva === 'geltona') {
+    console.log('Ruošiamės');
+} else if (spalva === 'zalia') {
+    console.log('Eik!');
+}
+
+switch (spalva) {
+    case 'raudona':
+        console.log('Stovime');
+        break;
+    case 'geltona':
+        console.log('Ruošiamės');
+        break;
+    case 'zalia':
+        console.log('Eik!');
+}
+
+console.clear();
+
+let box = rand(1, 4);
+let size;
+if(box === 1) {
+    size = 'S';
+}
+if(box === 2) {
+    size = 'M';
+}
+if(box === 3) {
+    size = 'L';
+}
+if(box === 4) {
+    size = 'XL';
+}
+
+console.log('Box:', size);
+
+if (size === 'S') {
+    console.log('Tikriname S');
+}
+if (size === 'M' || size === 'S') {
+    console.log('Tikriname M');
+}
+if (size === 'L' || size === 'M' || size === 'S') {
+    console.log('Tikriname L');
+}
+if (size === 'XL' || size === 'L' || size === 'M' || size === 'S') {
+    console.log('Tikriname XL');
+}
+
+
+// for (let i = 0; i < 4; i++) {
+
+    switch (size) {
+        case 'S':
+            console.log('Tikriname S');
+        case 'M':
+            console.log('Tikriname M');
+        case 'L':
+            console.log('Tikriname L');
+        case 'XL':
+            console.log('Tikriname XL');
+    }
+
+// }
+
+console.clear();
+
+let vidinis;
+let kartai = 0;
+let kartaiDidelis = 0;
+let kartaiMazas = 0;
+
+do {
+    kartaiDidelis++;
+    vidinis = rand(5, 10);
+
+    if (vidinis === 5) {
+        kartai++
+    } else {
+        kartai = 0;
+    }
+    
+    console.log('Vidinis:', vidinis);
+
+    for (let i = 0; i < vidinis; i++) {
+        kartaiMazas++;
+        // console.log('A');
+    }
+
+} while (kartai != 3);
+
+console.log('Kartai didelis:', kartaiDidelis);
+console.log('Kartai mazas:', kartaiMazas);
