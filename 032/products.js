fd6bcdc73d286f5811d20329c90ca804d4425c87
A-Kija <arvydas.kijakauskas@gmail.com>
2024-04-24 15:37:59 +0300

    034

--- 032/products.js
+++ 032/products.js
@@ -20,6 +20,7 @@ window.addEventListener('load', _ => {
     const LAST_ID_LS = 'productsLastSavedId';
     const PRODUCTS_LS = 'productsList';
     let destroyId = 0;
+    let updateId = 0;
 
     const listHtml = document.querySelector('.--list');
     const closeButtons = document.querySelectorAll('.--close');
@@ -31,7 +32,11 @@ window.addEventListener('load', _ => {
     
     //delete
     const deleteModal = document.querySelector('.modal--delete');
-    const deleteButton = deleteModal.querySelector('.--submit');
+    const destroyButton = deleteModal.querySelector('.--submit');
+
+    //edit
+    const editModal = document.querySelector('.modal--edit');
+    const updateButton = editModal.querySelector('.--submit');
     
     
     // LS functions
@@ -71,6 +76,11 @@ window.addEventListener('load', _ => {
         write(deleteData);
     }
 
+    const updateData = (id, data) => {
+        const updateData = read().map(p => p.id == id ? {...data, id} : p);
+        write(updateData);
+    }
+
     // LS functions
 
 
@@ -97,11 +107,19 @@ window.addEventListener('load', _ => {
         });
         listHtml.innerHTML = productsHtml;
         registerDelete();
+        registerEdit();
     }
 
     const prepareDeleteModal = id => {
         const title = read().find(p => p.id == id).productTitle;
         deleteModal.querySelector('.product--title').innerText = title;
+    }
+
+    const prepareEditModal = id => {
+        const product = read().find(p => p.id == id);
+        editModal.querySelectorAll('[name]').forEach(i => {
+            i.value = product[i.getAttribute('name')];
+        });
     }
 
     // DOM
@@ -131,6 +149,13 @@ window.addEventListener('load', _ => {
         showList(); // DOM
     }
 
+    const update = _ => {
+        const data = getDataFromForm(editModal); // CRUD
+        updateData(updateId, data); // LS
+        hideModal(editModal); // DOM
+        showList(); // DOM
+    }
+
     // CRUD
 
 
@@ -141,8 +166,19 @@ window.addEventListener('load', _ => {
         document.querySelectorAll('.--delete').forEach(b => {
             b.addEventListener('click', _ => {
                 showModal(deleteModal);
-                prepareDeleteModal(b.value);
+                prepareDeleteModal(parseInt(b.value));
                 destroyId = parseInt(b.value);
+            });
+        });
+    }
+
+
+    const registerEdit = _ => {
+        document.querySelectorAll('.--edit').forEach(b => {
+            b.addEventListener('click', _ => {
+                showModal(editModal);
+                prepareEditModal(parseInt(b.value));
+                updateId = parseInt(b.value);
             });
         });
     }
@@ -150,6 +186,17 @@ window.addEventListener('load', _ => {
 
 
     // Events
+
+
+    // DEV
+
+    const devButton = document.querySelector('.seed');
+    devButton.addEventListener('click', _ => {
+        seed();
+        showList();
+    })
+
+    // DEV
 
 
 
@@ -167,13 +214,37 @@ window.addEventListener('load', _ => {
 
     storeButton.addEventListener('click', _ => store());
 
-    deleteButton.addEventListener('click', _ => destroy());
+    destroyButton.addEventListener('click', _ => destroy());
+
+    updateButton.addEventListener('click', _ => update());
 
     // Events
 
     setTimeout(_ => showList(), 2000);
     
 
+
+
+
+
+// LS data
+
+const seedData = [
+    {id: 1, productTitle: 'Didelis stalas', productPrice: '50.7', productDesc: 'Kažkoks didelis medinis stalas'},
+    {id: 2, productTitle: 'Stalas', productPrice: '30.7', productDesc: 'Kažkoks medinis stalas'},
+    {id: 3, productTitle: 'Kėdė', productPrice: '10.7', productDesc: 'Kažkokia medinė kėdė'},
+    {id: 4, productTitle: 'Fotelis', productPrice: '100.47', productDesc: 'Labai patogus fotelis'},
+    {id: 5, productTitle: 'Sofa', productPrice: '300.7', productDesc: 'Kažkokia sofa'},
+    {id: 6, productTitle: 'Lova', productPrice: '200.7', productDesc: 'Kažkokia lova'},
+    {id: 7, productTitle: 'Spinta', productPrice: '150.7', productDesc: 'Kažkokia spinta'},
+    {id: 8, productTitle: 'Kampinė spinta', productPrice: '250.7', productDesc: 'Kažkokia kampinė spinta'},
+    {id: 9, productTitle: 'Kampinė sofa', productPrice: '400.7', productDesc: 'Kažkokia kampinė sofa'},
+    {id: 10, productTitle: 'Kampinė lova', productPrice: '300.7', productDesc: 'Kažkokia kampinė lova'}
+];
+
+const seed = _ => {
+    write(seedData);
+    localStorage.setItem(LAST_ID_LS, 10);
+}
+
 });
-
-
