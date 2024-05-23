784fff45fe0bdc854447241c5a210fe7bca8a527
A-Kija <arvydas.kijakauskas@gmail.com>
2024-05-23 15:31:19 +0300

    051

--- db/app.js
+++ db/app.js
@@ -5,18 +5,55 @@ const mysql = require('mysql');
 const app = express();
 const port = 8080;
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'forest'
+});
+
+connection.connect();
+
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
 
     let html = fs.readFileSync('./data/index.html', 'utf8');
     const listItem = fs.readFileSync('./data/listItem.html', 'utf8');
 
-    res.send(html);
+    // SELECT column1, column2, ...
+    // FROM table_name;
+
+    const sql = `
+        SELECT id, name, height, type
+        FROM trees
+        -- WHERE height > 5
+        ORDER BY type, height DESC
+    `;
+
+    connection.query(sql, (err, rows) => {
+        if (err) throw err;
+
+        let listItems = '';
+        rows.forEach(tree => {
+            let liHtml = listItem;
+            liHtml = liHtml
+            .replace('{{ID}}', tree.id)
+            .replace('{{NAME}}', tree.name)
+            .replace('{{HEIGHT}}', tree.height)
+            .replace('{{TYPE}}', tree.type);
+            listItems += liHtml;
+        });
+        html = html.replace('{{LI}}', listItems)
+        res.send(html);
+    });
 });
+
+
+
 
 
 app.listen(port, _ => {
