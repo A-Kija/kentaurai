fd89d2f57c196bdeef139cc140ad117f86974cdd
A-Kija <arvydas.kijakauskas@gmail.com>
2024-06-11 15:38:27 +0300

    007

--- r1/react1/src/App.jsx
+++ r1/react1/src/App.jsx
@@ -8,9 +8,28 @@ function App() {
 
     const [countLetter, setCountLetter] = useState(3);
     const [letters, setLetters] = useState('');
-    const [sq, setSq] = useState([]);
+    // const [sq, setSq] = useState(_ => JSON.parse(localStorage.getItem('sq') ?? '[]'));
+    const [sq, setSq] = useState(null);
 
-    const makeLetters = useCallback( _ => {
+
+    useEffect(_ => {
+
+        setTimeout(_ => {
+            setSq(JSON.parse(localStorage.getItem('sq') ?? '[]'));
+        }, 2000);
+
+    }, [setSq]);
+
+
+    useEffect(_ => {
+        if (sq === null) {
+            return;
+        }
+        localStorage.setItem('sq', JSON.stringify(sq));
+    }, [sq]);
+
+
+    const makeLetters = useCallback(_ => {
         if (countLetter <= 5) {
             setLetters('A'.repeat(countLetter));
         } else {
@@ -22,10 +41,25 @@ function App() {
         setCountLetter(c => c + 1);
     }
 
+    // const makeSq = _ => {
+    //     setSq(s => {
+    //         const newSq = [{id: rand(1000000, 9999999)}, ...s];
+    //         localStorage.setItem('sq', JSON.stringify(newSq));
+    //         return newSq;
+    //     });
+    // }
     const makeSq = _ => {
-        setSq(s => [{id: rand(1000000, 9999999)}, ...s]);
+        setSq(s => [{ id: rand(1000000, 9999999) }, ...s]);
     }
-    
+
+    // const destroySq = id => {
+    //     setSq(s => {
+    //         const newSq = s.filter(sq => sq.id !== id);
+    //         localStorage.setItem('sq', JSON.stringify(newSq));
+    //         return newSq;
+    //     });
+    // }
+
     const destroySq = id => {
         setSq(s => s.filter(sq => sq.id !== id));
     }
@@ -34,9 +68,9 @@ function App() {
         makeLetters();
     }, [countLetter, makeLetters]);
 
-    useEffect(_ => {
-        console.log(sq.length);
-    }, [sq]);
+    // useEffect(_ => {
+    //     console.log(sq.length);
+    // }, [sq]);
 
 
 
@@ -46,7 +80,15 @@ function App() {
             <header className="App-header">
                 <div className="sq-bin">
                     {
-                        sq.map(s => <Sq key={s.id} sq={s} destroySq={destroySq} />)
+                        sq !== null
+                            ?
+                            sq.length
+                                ?
+                                sq.map(s => <Sq key={s.id} sq={s} destroySq={destroySq} />)
+                                :
+                                <div>No squares</div>
+                            :
+                            <div>Loading...</div>
                     }
                 </div>
                 <h1>{letters}</h1>
